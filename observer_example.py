4acd68acd0c410a50652927e30449522ed618255
Sergey Shevtsov <dev.serzh@gmail.com>
2020-02-18 23:04:42 +0300

    observer pattern

---
+++ observer_example.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+import requests
+
+
+class Publisher(ABC):
+    @abstractmethod
+    def subscribe(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def unsubscribe(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+
+
+class AvailabilityChecker(Publisher):
+    _state = None
+    _observers: List[Observer] = []
+
+    def subscribe(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def unsubscribe(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+        for observer in self._observers:
+            observer.update(self)
+
+    def check(self):
+        r = requests.get('https://example.com')
+        self._state = r.status_code
+        self.notify()
+
+
+class Observer(ABC):
+    def update(self, subject: Publisher) -> None:
+        pass
+
+
+class EventListenerA(Observer):
+    def update(self, subject: Publisher) -> None:
+        if subject._state == 200:
+            print('Site is available')
+        else:
+            print('Site is unavailable')
+
+
+class EventListenerB(Observer):
+    def update(self, subject: Publisher) -> None:
+        if subject._state == 200:
+            r = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+            print(r.json())
+        else:
+            print('Non internet connection')
+
+
+if __name__ == '__main__':
+    checker = AvailabilityChecker()
+    listener_a = EventListenerA()
+    listener_b = EventListenerB()
+
+    for listener in [listener_a, listener_b]:
+        checker.subscribe(listener)
+
+    checker.check()
